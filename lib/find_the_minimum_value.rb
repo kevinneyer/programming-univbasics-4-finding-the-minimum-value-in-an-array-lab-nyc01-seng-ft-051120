f17e510de1eed3106e57484be112cc45df309643
Kevin Neyer <kevinneyer@gmail.com>
2020-04-09 18:34:05 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def find_min_value(array)
-  # Add your solution here
+  index = array.length 
+  message = " "
+  
+  array.index do |f|
+    i = 1
+    if array[f] < array[i]
+      message = array[f]
+    end
+    i += 1 
+  end
+  message 
+  binding.pry 
 end
